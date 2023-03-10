7a659abb95699c5a17528b3d3caaf5c8f10e74d5
BabyPuppyCoin <125125422+BabyPuppyCoin@users.noreply.github.com>
2023-03-10 10:40:13 +0100

    Add files via upload

---
+++ PuppyLimited.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./ERC721A.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract PuppyLimited is Ownable, ERC721A, ReentrancyGuard {
+
+
+  constructor(
+    uint256 maxBatchSize_,
+    uint256 collectionSize_
+  ) ERC721A("Puppy Limited", "PUPPYLIMITED", maxBatchSize_, collectionSize_) {}
+
+  modifier callerIsUser() {
+    require(tx.origin == msg.sender, "The caller is another contract");
+    _;
+  }
+
+  function devMint(uint256 quantity) external onlyOwner {
+    require(
+      totalSupply() + quantity <= collectionSize,
+      "too many already minted before dev mint"
+    );
+    require(
+      quantity % maxBatchSize == 0,
+      "can only mint a multiple of the maxBatchSize"
+    );
+    uint256 numChunks = quantity / maxBatchSize;
+    for (uint256 i = 0; i < numChunks; i++) {
+      _safeMint(msg.sender, maxBatchSize);
+    }
+  }
+
+  // // metadata URI
+  string private _baseTokenURI;
+
+  function _baseURI() internal view virtual override returns (string memory) {
+    return _baseTokenURI;
+  }
+
+  function setBaseURI(string calldata baseURI) external onlyOwner {
+    _baseTokenURI = baseURI;
+  }
+
+
+  function numberMinted(address owner) public view returns (uint256) {
+    return _numberMinted(owner);
+  }
+
+  function getOwnershipData(uint256 tokenId)
+    external
+    view
+    returns (TokenOwnership memory)
+  {
+    return ownershipOf(tokenId);
+  }
+
+}
